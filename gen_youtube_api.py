2617d251a48135df491dc3764caed1523089753a
Celelibi <celelibi@gmail.com>
2025-04-26 10:06:57 +0200

    Generate the python API methods from description
    The description actually comes from the discovery API.
    
    Signed-off-by: Celelibi <celelibi@gmail.com>

---
+++ gen_youtube_api.py
@@ -0,0 +1,274 @@
+#!/usr/bin/env python3
+
+"""This program generates a YouTubeMixin class from the discovery API of
+google. All the methods have a docstring containing whatever documentation was
+available in the JSON from the API. All the arguments required by the Youtube
+Data API are also required by the python methods. All the method names and
+argument names are translated to the PEP 8 recommended coding style."""
+
+import re
+import sys
+
+import requests
+
+
+
+DISCOVERY_URL = "https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"
+
+
+
+def str_index_set(haystack, needles, start=None, end=None, /, **kwargs):
+    """Like str.index, but finds the first index in haystack where one of the
+    needed is located."""
+
+    idx = None
+    for needle in needles:
+        try:
+            i = haystack.index(needle, start, end)
+        except ValueError:
+            continue
+        if idx is None or idx > i:
+            idx = i
+
+    if idx is not None:
+        return idx
+    if "default" in kwargs:
+        return kwargs["default"]
+    raise ValueError("substring not found")
+
+
+
+def str_rindex_set(haystack, needles, start=None, end=None, /, **kwargs):
+    """Like str.rindex, but finds the last index in haystack where one of the
+    needed is located."""
+
+    idx = None
+    for needle in needles:
+        try:
+            i = haystack.rindex(needle, start, end)
+        except ValueError:
+            continue
+        if idx is None or idx < i:
+            idx = i
+
+    if idx is not None:
+        return idx
+    if "default" in kwargs:
+        return kwargs["default"]
+    raise ValueError("substring not found")
+
+
+
+def wrap(text, width=80, seps=" "):
+    """Wraps a text string to `width' columns on separators `seps'."""
+
+    if width < 1:
+        raise ValueError(f"Invalid width {width}, must be >= 1")
+
+    # This helps with special cases:
+    # - empty string should return [''], not []
+    # - when the remaining text is shorter than `width', we want to "split" at
+    #   the very end of the text.
+    text += seps[0]
+
+    ret = []
+    while text:
+        idx = str_rindex_set(text, seps, 0, width, default=None)
+        if idx is None:
+            idx = str_index_set(text, seps, width, default=len(text))
+
+        ret.append(text[:idx].rstrip(seps))
+        text = text[idx:].lstrip(seps)
+
+    return ret
+
+
+
+def format_docstr(s, levels=1):
+    """Format a multiline string to be a docstring.
+
+    Namely this function:
+    - Prepend and append three double-quote
+    - Wrap the long lines so that the text fit on 80 columns including the
+      indentation
+    - Lines starting with "- " have an added two spaces on the continuation
+      (see above and here)
+    - Prepend levels*4 spaces on each lines."""
+
+    w = levels * 4
+    sp = " " * w
+
+    retval = ""
+    for line in f'"""{s}"""'.splitlines():
+        wtmp = w
+        sptmp = sp
+
+        # Handle the list items specially. Add 2 spaces on continuation lines.
+        if line.startswith("- "):
+            l = wrap(line, 80 - w)[0]
+            if retval:
+                retval += "\n"
+            retval += sp + l
+            line = line.removeprefix(l).lstrip()
+            if line == "":
+                continue
+            wtmp += 2
+            sptmp += "  "
+
+        # Normal lines (not starting with "- " and continuations are handled here
+        for l in wrap(line, 80 - wtmp):
+            if retval:
+                retval += "\n"
+            if l:
+                retval += sptmp
+            retval += l
+
+    return retval
+
+
+
+def camel_case_to_python(camel):
+    """Convert a camelCase name into a snake_case name. Also prevent overriding
+    some python builtin by appending an underscore."""
+
+    newname = re.sub(r"([a-z])([A-Z]+)", r"\1_\2", camel).lower()
+    if hasattr(__builtins__, newname):
+        newname += "_"
+    return newname
+
+
+
+def gen_method_doc(meth, named_args, optional_args, params_style_conv):
+    """Generate the docstring of a method."""
+
+    params = meth["parameters"]
+
+    doc = meth["description"] + "\n\n"
+    if named_args:
+        doc += "Required arguments:\n"
+    for n in named_args:
+        o = params_style_conv.get(n, n)
+        if "description" in params[o]:
+            doc += f"- {n}: {params[o]['type']}: {params[o]['description']}\n"
+        else:
+            doc += f"- {n}: {params[o]['type']}\n"
+
+    if optional_args:
+        doc += "Optional arguments:\n"
+    for n in optional_args:
+        o = params_style_conv.get(n, n)
+        if "description" in params[o]:
+            doc += f"- {n}: {params[o]['type']}: {params[o]['description']}\n"
+        else:
+            doc += f"- {n}: {params[o]['type']}\n"
+
+    return format_docstr(doc, levels=2)
+
+
+
+def gen_code_for_method(meth, is_first, fp):
+    """Generate the code for a method as described in the JSON provided by the
+    discovery URL."""
+
+    meth_name = "_".join(meth["id"].split(".")[1:])
+    meth_name = camel_case_to_python(meth_name)
+
+    params = meth["parameters"]
+
+    # Conversion frm camelCase to snake_case pf parameters
+    params_style_conv = {camel_case_to_python(n): n for n in sorted(params)}
+    params_style_conv = {k: v for k, v in params_style_conv.items() if k != v}
+
+    # These are the arguments explicited required for the generated functions
+    required_params = {n: p for n, p in params.items() if p.get("required", False)}
+    named_args = meth["parameterOrder"]
+    named_args += sorted(set(required_params) - set(named_args))
+    optional_args = sorted(set(params) - set(named_args))
+
+    # Convert those names to pythonic snake_case
+    named_args = [camel_case_to_python(n) for n in named_args]
+    optional_args = [camel_case_to_python(n) for n in optional_args]
+
+    # Parameters passed by location require a special handling in the code
+    path_params = {n: p for n, p in params.items() if p["location"] == "path"}
+
+    # Build the strings used in the code
+    named_args_str = ", ".join(named_args)
+    if named_args_str:
+        named_args_str += ", "
+    named_args_dict = ", ".join(f"{n!r}: {n}" for n in named_args)
+    named_args_dict = "{" + named_args_dict + "}"
+
+    params_style_conv_str = "\n".join(" " * 12 + f"{k!r}: {v!r}," for k, v in params_style_conv.items())
+    params_style_conv_str = "{\n" + params_style_conv_str + "\n        }"
+
+    # The method and path to call
+    method = meth["httpMethod"].lower()
+    path = meth["flatPath"].removeprefix("youtube/v3/")
+
+    # Build the docstring
+    doc = gen_method_doc(meth, named_args, optional_args, params_style_conv)
+
+    # Finally, output the code
+    print(file=fp)
+    if not is_first:
+        print(file=fp)
+        print(file=fp)
+
+    print(f"    def {meth_name}(self, {named_args_str}request_args=None, **kwargs):", file=fp)
+    print(doc, file=fp)
+
+    print(file=fp)
+    print("        if request_args is None:", file=fp)
+    print("            request_args = {}", file=fp)
+    print(f"        kwargs.update({named_args_dict})", file=fp)
+    if params_style_conv:
+        print(f"        style_conv = {params_style_conv_str}", file=fp)
+        print("        kwargs = {style_conv.get(k, k): v for k, v in kwargs.items()}", file=fp)
+    if path_params:
+        print('        # pylint: disable=consider-using-f-string', file=fp)
+        print(f'        path = {path!r}.format(**kwargs)', file=fp)
+        print(f"        return self.{method}(path, params=kwargs)", file=fp)
+    else:
+        print(f"        return self.{method}({path!r}, params=kwargs, **request_args)", file=fp)
+
+
+
+# pylint: disable=missing-function-docstring
+def main():
+    if len(sys.argv) != 2:
+        print(f"usage: {sys.argv[0]} outputfilename")
+        return 1
+
+    outfile = sys.argv[1]
+    if "generated" not in outfile or not outfile.endswith(".py"):
+        print("Output filename must contain 'generated' end with .py", file=sys.stderr)
+        return 2
+
+    res = requests.get(DISCOVERY_URL, timeout=60)
+    res.raise_for_status()
+    desc = res.json()
+
+    with open(outfile, "w") as fp:
+        print(format_docstr("File generated with: " + " ".join(sys.argv), levels=0), file=fp)
+        print(file=fp)
+        print(f'class {desc["canonicalName"]}Mixin:', file=fp)
+        print("    # pylint: disable=too-many-public-methods,too-many-lines", file=fp)
+        print(format_docstr(f'{desc["title"]}\n\n{desc["description"]}'), file=fp)
+
+        first = True
+        for resname in sorted(desc["resources"]):
+            res = desc["resources"][resname]
+            if "methods" not in res:
+                print(f"Skipping the weirdly formatted resource {resname}:", res, file=sys.stderr)
+                continue
+
+            for method in res["methods"].values():
+                gen_code_for_method(method, first, fp)
+                first = False
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
